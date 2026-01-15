50e32720774cd6699bb6b6db63f6479eec4092af
Ashwin <ashwin@superadmins-MacBook-Pro-2.local>
2026-01-16 00:26:28 +0800

    Add mongoose schema and create data models.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-import products from './data/products.js';
 
+import products from './data/products.js';
+import connectDB from './config/db.js';
 const PORT = process.env.PORT || 5005;
 const app = express();
 
+connectDB();
 app.get('/', (req, res) => {
     res.send('Server is running...');
 });
