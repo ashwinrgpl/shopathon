270c467b697e6d54022c7863e0d2641507c2d8b4
Ashwin <ashwin@superadmins-MacBook-Pro-2.local>
2026-01-22 01:33:41 +0800

    Add auth middleware and protect routes.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-
+import cookieParser from 'cookie-parser';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import connectDB from './config/db.js';
@@ -9,8 +9,12 @@ import { notFound, errorHandler } from './middleware/errorMissleware.js';
 const PORT = process.env.PORT || 5005;
 const app = express();
 
+//Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//Cookie parser middleware
+app.use(cookieParser());
 
 connectDB();
 app.get('/', (req, res) => {
