173c3832cc3fb289556f54e73d1527f424575067
Ashwin <ashwin@superadmins-MacBook-Pro-2.local>
2026-01-29 00:39:02 +0800

    Create OrderScreen.

--- frontend/src/redux/slices/ordersApiSlice.js
+++ frontend/src/redux/slices/ordersApiSlice.js
@@ -10,7 +10,14 @@ const ordersApiSlice = apiSlice.injectEndpoints({
                 body: { ...order },
             }),
         }),
+        getOrderById: builder.query({
+            query: (id) => ({
+                url: `${ORDERS_URL}/${id}`,
+                method: 'GET',
+            }),
+            keepUnusedDataFor: 5,
+        }),
     }),
 });
 
-export const { useCreateOrderMutation } = ordersApiSlice;
+export const { useCreateOrderMutation, useGetOrderByIdQuery } = ordersApiSlice;
