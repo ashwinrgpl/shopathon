e68692649fbc7ee522d7b0aec494328a2c541de0
Ashwin <ashwin@superadmins-MacBook-Pro-2.local>
2026-02-03 01:44:23 +0800

    Update order delivery status and product list screen for admin.

--- frontend/src/redux/slices/productsApiSlice.js
+++ frontend/src/redux/slices/productsApiSlice.js
@@ -9,8 +9,21 @@ const productsApiSlice = apiSlice.injectEndpoints({
             }),
             keepUnusedDataFor: 5,
         }),
+        getProductDetails: builder.query({
+            query: (productId) => ({
+                url: `${PRODUCTS_URL}/${productId}`,
+            }),
+            keepUnusedDataFor: 5,
+        }),
+        createProduct: builder.mutation({
+            query: () => ({
+                url: PRODUCTS_URL,
+                method: 'POST',
+            }),
+            invalidatesTags: ['Product'],
+        }),
     }),
 });
 
-export const { useGetProductsQuery, useGetProductDetailsQuery } = productsApiSlice;
+export const { useGetProductsQuery, useGetProductDetailsQuery, useCreateProductMutation } = productsApiSlice;
 
